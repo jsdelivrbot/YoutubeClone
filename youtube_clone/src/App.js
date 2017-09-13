19ce32048a1aced1c765d4dbfec3588015e8af9b
Adrian Pearman <adrianpearman12@gmail.com>
2017-09-13 19:49:30 -0400

    update to index file to remove unneeded code and files. App.js updated to include component variables, style.css within the src folder

--- youtube_clone/src/App.js
+++ youtube_clone/src/App.js
@@ -1,21 +1,50 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search'
+
+const API_KEY = 'AIzaSyBKzq_vfG8WJcKVeikTyruzPNxVJs2tI5s';
+
+// const random = {Math.floor(Math.random()*({this.state.videos.length}-1)+1)}
+// console.log(random);
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
+
+     this.videoSearch('skateboards')
+  }
+
+  videoSearch(term){
+    YTSearch({ key: API_KEY, term: term},(videos)=>{
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       })
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    const videoSearch = _.debounce((term)=>{this.videoSearch(term)},300);
+
+    return(
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos}/>
       </div>
     );
   }
 }
 
-export default App;
+export default App
