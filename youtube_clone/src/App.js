d1d210ecf5af3236bbc73e82b3b013a027d97dc5
Adrian Pearman <adrianpearman12@gmail.com>
2017-09-13 20:33:46 -0400

    files momved to style.css file for styling. App.css, manifest.json & favicon.ico deleted as no longer needed.

--- youtube_clone/src/App.js
+++ youtube_clone/src/App.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
